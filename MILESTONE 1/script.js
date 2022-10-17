f6e48309c234f4969d209cd56ea78ff14176e5d8
SainasFede <sainasfede@gmail.com>
2022-10-17 16:47:37 +0200

    add

---
+++ MILESTONE 1/script.js
@@ -0,0 +1,26 @@
+const imagesArray = [
+  '01.jpg',
+  '02.jpg',
+  '03.jpg',
+  '04.jpg',
+  '05.jpg'
+];
+
+let imagesContainer = '';
+
+const container = document.querySelector('container-image');
+
+
+for(let i = 0; i < imagesArray.length; i++){
+  imagesContainer += `
+  <img class="item" src="${imagesArray[i]}" alt="${imagesArray[i]}"> `;
+}
+let counterImages = 0;
+
+
+container.innerHTML = imagesContainer;
+
+const items = document.getElementsByClassName('item');
+
+items[counterImages].classList.add('active');
+
